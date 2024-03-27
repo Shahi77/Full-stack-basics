517d84d34f5cbf2f8aa8cdf1db41df4def64be3f
Shahi77 <shahiinfinity02@gmail.com>
2024-03-27 11:00:24 +0530

    connection between frontend and backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import axios from "axios";
 function App() {
   const [jokes, setJokes] = useState([]);
-
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
   return (
     <>
       <h1>Full stack </h1>
       <p>JOKES:{jokes.length}</p>
-      {jokes.map((joke, index) => {
+      {jokes.map((joke, index) => (
         <div key={joke.id}>
           <h3>{joke.first_name}</h3>
           <h3>{joke.last_name}</h3>
-        </div>;
-      })}
+        </div>
+      ))}
     </>
   );
 }
